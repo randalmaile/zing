66dd6ba5c3e6894dfe17fd7f87d13a48de522667
randalmaile <randalmaile@gmail.com>
2014-04-20 23:09:52 -0700

    updated js config

---
+++ source/javascripts/all.js
@@ -0,0 +1,2 @@
+//= require html5_scripts/zingchart-html5-min
+//= require config
---
+++ source/javascripts/config.js
@@ -0,0 +1,8 @@
+var myChart = {
+  type   : "line",
+  title  : {text: "Hello ZingChart World!"},
+  series : [
+    {values:[5, 10, 15, 5, 10, 5]},
+    {values:[2, 4, 6, 8, 10, 12]}
+  ]
+};
