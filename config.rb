04593d700d884c7fa127a8a5d96c3986ea794ffc
randalmaile <randalmaile@gmail.com>
2014-04-20 19:06:59 -0700

    Rake, config + zing-chart assets

--- config.rb
+++ config.rb
@@ -33,12 +33,24 @@
 ###
 
 # Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+set :file_watcher_ignore, [
+    /^\.idea\//,
+    /^\.bundle\//,
+    /^\.sass-cache\//,
+    /^\.git\//,
+    /^\.gitignore$/,
+    /\.DS_Store/,
+    /^build\//,
+    /^\.rbenv-.*$/,
+    /^Gemfile$/,
+    /^Gemfile\.lock$/,
+    /~$/,
+    /(^|\/)\.?#/
+]
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -56,13 +68,13 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
